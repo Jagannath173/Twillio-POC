41cb6fd08616ad5c3de846aa1a2bb087aaca2fd6
Jagannath173 <Jagannath.Patro@bilvantis.io>
2026-01-03 02:26:19 +0530

    Comment out logging of generated token in createToken function

--- server/src/controllers/video.controller.js
+++ server/src/controllers/video.controller.js
@@ -8,6 +8,6 @@ exports.createToken = (req, res) => {
     }
 
     const token = generateVideoToken(identity, room);
-    console.log("Generated token:", token);
+    // console.log("Generated token:", token);
     res.json({ token });
 };
